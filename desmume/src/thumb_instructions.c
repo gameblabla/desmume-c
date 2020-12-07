e68de55739975b3c1aaf26dfc38fe6ba67bd0b6b
gameblabla <gameblabla@protonmail.com>
2020-12-07 04:32:26 +0100

    Fix unaligned access. Fixes Rayman DS intros.

--- desmume/src/thumb_instructions.c
+++ desmume/src/thumb_instructions.c
@@ -40,10 +40,10 @@ extern BOOL execute;
 	#define READ8(a,b)		cpu->mem_if->read8(a,b)
 	#define WRITE8(a,b,c)	cpu->mem_if->write8(a,b,c)
 #else
-	#define READ32(a,b)		MMU_read32(cpu->proc_ID, b)
-	#define WRITE32(a,b,c)	MMU_write32(cpu->proc_ID,b,c)
-	#define READ16(a,b)		MMU_read16(cpu->proc_ID, b)
-	#define WRITE16(a,b,c)	MMU_write16(cpu->proc_ID,b,c)
+	#define READ32(a,b)		MMU_read32(cpu->proc_ID, b & 0xFFFFFFFC)
+	#define WRITE32(a,b,c)	MMU_write32(cpu->proc_ID,b & 0xFFFFFFFC,c)
+	#define READ16(a,b)		MMU_read16(cpu->proc_ID, b & 0xFFFFFFFE)
+	#define WRITE16(a,b,c)	MMU_write16(cpu->proc_ID,b & 0xFFFFFFFE,c)
 	#define READ8(a,b)		MMU_read8(cpu->proc_ID, b)
 	#define WRITE8(a,b,c)	MMU_write8(cpu->proc_ID,b,c)
 #endif
